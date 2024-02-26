51940b82c507b5d58cd8392d522dbc47211c75c9
saadfrhan <saadfarhan347@gmail.com>
2024-02-26 20:40:37 +0500

    update

--- app/lolly/[slug]/page.tsx
+++ app/lolly/[slug]/page.tsx
@@ -9,73 +9,75 @@ import ShareButton from "@/components/share-button";
 export const revalidate = 1;
 
 export default async function Page({
-  params,
-  searchParams,
+	params,
+	searchParams,
 }: {
-  params: { slug: string };
-  searchParams: {
-    view: "public";
-  };
+	params: { slug: string };
+	searchParams: {
+		view: "public";
+	};
 }) {
-  async function getLolly(slug: string) {
-    const result = await client.query<{
-      data: {
-        flavorTop: string;
-        flavorMiddle: string;
-        flavorBottom: string;
-        from: string;
-        message: string;
-        slug: string;
-        to: string;
-      };
-    }>(q.Get(q.Match(q.Index("lolly_by_slug"), slug)));
-    return result.data;
-  }
+	async function getLolly(slug: string) {
+		const result = await client.query<{
+			data: {
+				flavorTop: string;
+				flavorMiddle: string;
+				flavorBottom: string;
+				from: string;
+				message: string;
+				slug: string;
+				to: string;
+			};
+		}>(q.Get(q.Match(q.Index("lolly_by_slug"), slug)));
+		return result.data;
+	}
 
-  const lollyData = await getLolly(params.slug);
-  const link = `https://virtual-lollypop.vercel.app/lolly/${lollyData.slug}?view=to`;
+	const lollyData = await getLolly(params.slug);
+	const link = `https://virtual-lollypop.vercel.app/lolly/${lollyData.slug}?view=to`;
 
-  return (
-    <div>
-      <div className="rounded-lg shadow-md flex max-md:flex-col justify-center min-h-screen items-center gap-14 container w-full">
-        <Lolly
-          flavourTop={lollyData.flavorTop}
-          flavourMiddle={lollyData.flavorMiddle}
-          flavourBottom={lollyData.flavorBottom}
-        />
-        <div className="space-y-2">
-          <Card className="w-[350px]">
-            <CardContent className="py-6 space-y-2">
-              <div>
-                <p className="text-xs font-bold text-muted-foreground">FROM</p>
-                <p className="text-lg font-bold">{lollyData.from}</p>
-              </div>
-              <div>
-                <p className="text-xs font-bold text-muted-foreground">TO</p>
-                <p className="text-lg font-bold">{lollyData.to}</p>
-              </div>
-              <div>
-                <p className="text-xs font-bold text-muted-foreground">
-                  MESSAGE
-                </p>
-                <p className="text-base">{lollyData.message}</p>
-              </div>
-            </CardContent>
-          </Card>
-          {searchParams.view !== "public" && (
-            <ShareButton slug={lollyData.slug} />
-          )}
-        </div>
-      </div>
-      <Link
-        href="/create-lolly"
-        className={buttonVariants({
-          variant: "link",
-          className: "text-center flex w-full",
-        })}
-      >
-        Want to create your own?
-      </Link>
-    </div>
-  );
+	return (
+		<div>
+			<div className="rounded-lg shadow-md flex max-md:flex-col justify-center min-h-screen items-center gap-14 container w-full">
+				<Lolly
+					flavourTop={lollyData.flavorTop}
+					flavourMiddle={lollyData.flavorMiddle}
+					flavourBottom={lollyData.flavorBottom}
+				/>
+				<div className="space-y-2">
+					<div className="border-black border-2 rounded-sm">
+						<div className="max-w-sm mx-auto relative bg-[rgba(204,_204,_204,_0.5)]  rounded-sm shadow-md overflow-hidden">
+							<div className="p-5">
+								<div className="mb-3">
+									<p className="text-lg font-semibold text-white">From</p>
+									<p className="text-xl font-bold text-gray-800">{lollyData.from}</p>
+								</div>
+								<div className="mb-3">
+									<p className="text-lg font-semibold text-white">To</p>
+									<p className="text-xl font-bold text-gray-800">{lollyData.to}</p>
+								</div>
+								<div className="mb-3">
+									<p className="text-lg font-semibold text-white">Message</p>
+									<p className="text-md text-gray-600">{lollyData.message}</p>
+								</div>
+							</div>
+						</div>
+
+
+					</div>
+					{searchParams.view !== "public" && (
+						<ShareButton slug={lollyData.slug} />
+					)}
+				</div>
+			</div>
+			<Link
+				href="/create-lolly"
+				className={buttonVariants({
+					variant: "link",
+					className: "text-center flex w-full",
+				})}
+			>
+				Want to create your own?
+			</Link>
+		</div>
+	);
 }
--- components/flavours-drawer.tsx
+++ components/flavours-drawer.tsx
@@ -1,10 +1,10 @@
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Button } from "./ui/button";
 import { Lollipop, Popsicle, X } from "lucide-react";
@@ -14,46 +14,45 @@ import { Card, CardContent } from "./ui/card";
 import Link from "next/link";
 
 export default function FlavoursDrawer() {
-  return (
-    <Drawer>
-      <DrawerTrigger>
-        <Button
-          size="icon"
-          className="rounded-full fixed bottom-5 left-5"
-          variant="outline"
-        >
-          <Popsicle className="h-6 w-6" />
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent className="h-full">
-        <DrawerHeader className="flex w-full justify-between">
-          <DrawerTitle>Pre-made Flavours</DrawerTitle>
-          <DrawerClose>
-            <Button size="icon" variant="ghost" className="rounded-full">
-              <X />
-            </Button>
-          </DrawerClose>
-        </DrawerHeader>
-        <div className="grid grid-cols-6 max-[400px]:grid-cols-1 max-sm:grid-cols-2 max-md:grid-cols-3 max-lg:grid-cols-4 max-xl:grid-cols-5 gap-2 m-2 overflow-auto">
-          {lollipops.map((lollipop, index) => (
-            <Link key={index} href={`/create-lolly?flavour=${index}`}>
-              <DrawerClose className="w-full">
-                <Card>
-                  <CardContent className="flex flex-col gap-2 hover:bg-secondary duration-300 pt-6 items-center">
-                    <Lolly
-                      flavourBottom={lollipop.flavourBottom}
-                      flavourMiddle={lollipop.flavourMiddle}
-                      flavourTop={lollipop.flavourTop}
-                      className="h-[200px]"
-                    />
-                    <p>{lollipop.name}</p>
-                  </CardContent>
-                </Card>
-              </DrawerClose>
-            </Link>
-          ))}
-        </div>
-      </DrawerContent>
-    </Drawer>
-  );
+	return (
+		<Drawer>
+			<DrawerTrigger>
+				<Button
+					size="icon"
+					className="rounded-full fixed bottom-5 left-5"
+				>
+					<Popsicle className="h-6 w-6" />
+				</Button>
+			</DrawerTrigger>
+			<DrawerContent className="h-full border-0 bg-transparent">
+				<DrawerHeader className="flex w-full text-white mx-2 text-left justify-between">
+					<DrawerTitle>Pre-made Flavours</DrawerTitle>
+					<DrawerClose>
+						<Button size="icon" variant="ghost" className="rounded-full">
+							<X />
+						</Button>
+					</DrawerClose>
+				</DrawerHeader>
+				<div className="grid grid-cols-6 max-[400px]:grid-cols-1 max-sm:grid-cols-2 max-md:grid-cols-3 max-lg:grid-cols-4 max-xl:grid-cols-5 gap-2 m-2 overflow-auto">
+					{lollipops.map((lollipop, index) => (
+						<Link key={index} href={`/create-lolly?flavour=${index}`}>
+							<DrawerClose className="w-full">
+								<Card className="h-96 bg-transparent  border-0">
+									<CardContent className="flex flex-col  h-full gap-2  duration-300 pt-6 items-center">
+										<Lolly
+											flavourBottom={lollipop.flavourBottom}
+											flavourMiddle={lollipop.flavourMiddle}
+											flavourTop={lollipop.flavourTop}
+											className="h-[300px]"
+										/>
+										<p className="text-white">{lollipop.name}</p>
+									</CardContent>
+								</Card>
+							</DrawerClose>
+						</Link>
+					))}
+				</div>
+			</DrawerContent>
+		</Drawer>
+	);
 }
